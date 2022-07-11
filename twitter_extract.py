12a051d26e2040afb8161b3ccc8d5b80a71b850e
Mativas <76075521+MatiasVasquezOrrego@users.noreply.github.com>
2022-07-11 04:24:53 -0400

    add: error handling to the api request

--- twitter_extract.py
+++ twitter_extract.py
@@ -5,7 +5,7 @@ import datetime as dt
 from pathlib import Path
 
 TODAY = dt.datetime.today().isoformat(timespec = 'seconds') + 'Z'
-ONE_WEEK_AGO = (dt.datetime.today() - dt.timedelta(days = 6)).isoformat(timespec = 'seconds') + 'Z'
+ONE_DAY_AGO = (dt.datetime.today() - dt.timedelta(days = 1)).isoformat(timespec = 'seconds') + 'Z'
 
 def create_headers():
     bearer_token = os.environ.get('TWITTER_BEARER_TOKEN')
@@ -14,7 +14,7 @@ def create_headers():
 def create_params(hashtag):
     params = {
         'query': f'{hashtag} -is:retweet',
-        'start_time' : ONE_WEEK_AGO,
+        'start_time' : ONE_DAY_AGO,
         'end_time' : TODAY,
         'max_results': 100,
         'tweet.fields': 'author_id,conversation_id,created_at'
@@ -23,12 +23,14 @@ def create_params(hashtag):
 
 def get_search_request(headers, params):
     url = 'https://api.twitter.com/2/tweets/search/recent'
-    return requests.request("GET", url, params = params, headers = headers).json()
+    response = requests.request("GET", url, params = params, headers = headers)
+    if response.status_code != 200: return {'meta' : {'next_token' : None}, 'data' : []}
+    return response.json()
 
 def pagination(first_response, headers, params):
     response = first_response
     tweets = first_response['data']
-    while response['meta']['next_token']:
+    while response['meta'].get('next_token'):
         print(f"Total scrapped tweets: {len(tweets)}")
         params['pagination_token'] = response['meta']['next_token']
         response = get_search_request(headers, params)
@@ -39,18 +41,23 @@ def main():
     hashtags = ['AprueboFeliz', 'Apruebo', 'NuevaConstitucion', 'Plebiscito', 'NuevaConstitucion', 
         'RutaConstituyente', 'ConvencionConstituyente', 'PlebiscitoConstitucional', 'PlebiscitoDeSalida',
         'RechazoDeSalida', 'AprueboCrece', 'Constitucion', 'Rechazo', 'RechazoTransversal', 'YoRechazo']
-    tweets = []
+    total_tweets_df = pd.DataFrame()
     for hashtag in hashtags:
         print(f'Scrapping hashtag: {hashtag}')
         headers = create_headers()
         params = create_params(hashtag = hashtag)
         response = get_search_request(headers = headers, params = params)
-        tweets += pagination(first_response = response, headers = headers, params = params)
-    return tweets
+        tweets = pagination(first_response = response, headers = headers, params = params)
+        tweets_df = pd.DataFrame(tweets)
+        tweets_df['search_hashtag'] = hashtag
+        total_tweets_df = pd.concat([total_tweets_df, tweets_df])
+    return total_tweets_df
+
+def save_dataframe(df, name):
+    path = Path('data') / name
+    with open(path, 'w', newline = '') as f:
+        df.to_csv(f, index = False)
 
 if __name__ == '__main__':
-    tweets = main()
-    tweets_df = pd.DataFrame(tweets)
-    path = Path('data')
-    with open(path, 'w', newline = '') as f:
-        tweets_df.to_csv(f, index = False)
+    total_tweets_df = main()
+    save_dataframe(total_tweets_df, 'tweets.csv')
